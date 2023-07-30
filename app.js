a6ca2588415706d4eeaa3120233b85cd57ba4496
Nihar4 <niharpatel4444@gmail.com>
2023-07-31 01:14:39 +0530

    storage change

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ config({
 const app = express();
 
 // Using Middlewares
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 app.use(
     express.urlencoded({
+        limit: '100mb',
         extended: true,
     })
 );
-app.use(cookieParser());
+app.use(cookieParser({ limit: '100mb' }));
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,
